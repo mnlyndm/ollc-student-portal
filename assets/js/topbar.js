bfa7ddfb7fa186f6d2e39f16b9eba6abccf771ad
mnlyndm <millaminaminalyn@gmail.com>
2023-11-20 11:23:44 +0800

    Inserting topbar content Modified JS is responsible for fetching the content of an external HTML file (topbar.html), inserting that content into an HTML element with the ID topbarContainer, and providing dropdown toggle functionality to two specific button-dropdown pairs (admissionBtn-admissionDropdown and aboutBtn-aboutDropdown).

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,17 +1,21 @@
-function toggleDropdown(btnId, dropdownId) {
-    const button = document.getElementById(btnId);
-    const dropdown = document.getElementById(dropdownId);
+fetch('./topbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('topbarContainer').innerHTML = data;
+        function toggleDropdown(btnId, dropdownId) {
+            const button = document.getElementById(btnId);
+            const dropdown = document.getElementById(dropdownId);
 
-    button.addEventListener('click', function () {
-        dropdown.classList.toggle('hidden');
-    });
-
-    window.addEventListener('click', function (event) {
-        if (!event.target.closest('#' + btnId) && !event.target.closest('#' + dropdownId)) {
-            dropdown.classList.add('hidden');
+            button.addEventListener('click', function () {
+                dropdown.classList.toggle('hidden');
+            })
+            window.addEventListener('click', function (event) {
+                if (!event.target.closest('#' + btnId) && !event.target.closest('#' + dropdownId)) {
+                    dropdown.classList.add('hidden');
+                }
+            });
         }
-    });
-}
 
-toggleDropdown('admissionBtn', 'admissionDropdown');
-toggleDropdown('aboutBtn', 'aboutDropdown');
+    toggleDropdown('admissionBtn', 'admissionDropdown');
+    toggleDropdown('aboutBtn', 'aboutDropdown');
+});
