b978817173b3625092fd8e989397aa6402d540db
mnlyndm <millaminaminalyn@gmail.com>
2023-11-23 20:44:40 +0800

    Some functions added: Function to save the generated ID and pwd in .txt using Blob and URL.createObjectURL Function of Email Validation Funtion of Required field Function of Popup message Pop up message design added

--- assets/js/pre-registration.js
+++ assets/js/pre-registration.js
@@ -1,9 +1,25 @@
 function generateStudentIdAndPwd() {
 
+    var courseIframe = document.getElementById('courseIframe');
+    var selectedCourse = courseIframe.contentWindow.document.getElementById('course').value;
+
+    if (!selectedCourse) {
+        alert("Please select a course before submitting the form.");
+        return;
+    }
+
+    var requiredFields = document.querySelectorAll('input[required]');
+    for (var i = 0; i < requiredFields.length; i++) {
+        if (!requiredFields[i].value) {
+            alert("Please fill out all required fields before submitting the form.");
+            return;
+        }
+    }
+
     if (document.getElementById('submit').disabled) {
         alert("You have already submitted the form.");
         return;
-    }
+    }     
 
     var stdSurname = document.getElementById('stdSurname').value;
     var stdFirstname = document.getElementById('stdFirstname').value;
@@ -23,9 +39,21 @@ function generateStudentIdAndPwd() {
 
     var password = generateRandomPassword(8);
 
-    document.getElementById('generatedStdID').innerText = "Generated Student ID: " + studentId;
-    document.getElementById('generatedPwd').innerText = "Generated Password: " + password;
+    document.getElementById('generatedStdID').innerText = studentId;
+    document.getElementById('generatedPwd').innerText = password;
 
+    var blob = new Blob(["Student ID: " + studentId + "\nPassword: " + password], { type: "text/plain" });
+
+    var a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = studentId + ".txt";
+        a.click();
+
+        document.getElementById('popup').style.display = 'block';
+}
+
+function btnPopup() {
+    window.location.href = '../student/login.html';
 }
 
 function getAlphabetPosition(letter) {
@@ -40,4 +68,17 @@ function generateRandomPassword(length) {
         password += charset.charAt(randomIndex);
     }
     return password;
+}
+function validateEmail() {
+    var emailInput = document.getElementById("email");
+    var emailError = document.getElementById("emailError");
+
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var isValidEmail = emailPattern.test(emailInput.value);
+
+    if (!isValidEmail) {
+        emailError.classList.remove("hidden");
+    } else {
+        emailError.classList.add("hidden");
+    }
 }
