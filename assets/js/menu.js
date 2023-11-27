03a575fb4d6b9264c32210fd5a351d473ecd26e5
mnlyndm <millaminaminalyn@gmail.com>
2023-11-27 17:20:17 +0800

    Modified: Select option using const not iframe Removing generate id and password, popup, and blob functions for a while

---
+++ assets/js/menu.js
@@ -0,0 +1,81 @@
+const suffixes = ["-- select one --","JR","SR","I","II","III","IV",];
+const courses = [
+    "-- select one --",
+    "Bachelor of Science in Information Technology",
+    "Bachelor of Science in Hospitality Management",
+    "Bachelor of Science in Criminology",
+    "Bachelor of Science in Business Administration",
+    "Bachelor of Education Major in English",
+    "Bachelor of Education Major in Math"
+];
+const cities = [
+    "-- select one --",
+    "Manila",
+    "Quezon City",
+    "Davao",
+    "Caloocan City",
+    "Canagatan",
+    "Taguig City",
+    "Pasig City",
+    "Valenzuela",
+    "City of Parañaque",
+    "Bacoor",
+    "Tondo",
+    "Las Piñas City",
+    "Pasay City",
+    "Mandaluyong City",
+    "Malabon",
+    "San Pedro",
+    "Navotas",
+    "Santa Ana",
+    "General Mariano Alvarez",
+    "Payatas",
+    "San Andres",
+    "Santa Cruz",
+    "San Juan",
+    "Poblacion",
+    "Santamesa",
+    "Bagong Silangan",
+    "Putatan",
+    "Western Bicutan",
+    "Banco Filipino International Village",
+    "Paco",
+    "Malate",
+    "Pandacan",
+    "San Isidro",
+    "San Antonio",
+    "Pateros",
+    "Tatalon",
+    "Sucat",
+    "Don Bosco",
+    "Lower Bicutan",
+    "Bignay",
+    "Bagumbayan",
+    "Upper Bicutan",
+    "Marikina Heights",
+    "Central Signal Village",
+    "Bayanan",
+    "Karuhatan",
+    "Bel-Air",
+    "Santo Niño",
+    "Pansol",
+    "Baclaran",
+    "West Rembo",
+    "Bagong Pag-Asa",
+    "Pinyahan"
+];
+
+function populateSelect(selectId, options) {
+    const selectElement = document.getElementById(selectId);
+
+    options.forEach(option => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option;
+        optionElement.text = option;
+        selectElement.add(optionElement);
+    });
+}
+
+populateSelect("suffixMenu", suffixes);
+populateSelect("courseMenu", courses);
+populateSelect("cityMenu", cities);
--- assets/js/pre-registration.js
+++ assets/js/pre-registration.js
@@ -1,80 +1,9 @@
-function generateStudentIdAndPwd() {
-
-    var courseIframe = document.getElementById('courseIframe');
-    var selectedCourse = courseIframe.contentWindow.document.getElementById('course').value;
-
-    if (!selectedCourse) {
-        alert("Please select a course before submitting the form.");
-        return;
-    }
-
-    var requiredFields = document.querySelectorAll('input[required]');
-    for (var i = 0; i < requiredFields.length; i++) {
-        if (!requiredFields[i].value) {
-            alert("Please fill out all required fields before submitting the form.");
-            return;
-        }
-    }
-
-    if (document.getElementById('submit').disabled) {
-        alert("You have already submitted the form.");
-        return;
-    }     
-
-    var stdSurname = document.getElementById('stdSurname').value;
-    var stdFirstname = document.getElementById('stdFirstname').value;
-
-    document.getElementById('submit').disabled = true;
-
-    var currentDate = new Date();
-    var studentId =
-        currentDate.getFullYear().toString().substr(-2) +
-        ('0' + (currentDate.getMonth() + 1)).slice(-2) +
-        ('0' + currentDate.getDate()).slice(-2) +
-        ('0' + currentDate.getHours()).slice(-2) +
-        ('0' + currentDate.getMinutes()).slice(-2) +
-        ('0' + currentDate.getSeconds()).slice(-2) +
-        getAlphabetPosition(stdFirstname.charAt(0)) +
-        getAlphabetPosition(stdSurname.charAt(0));
-
-    var password = generateRandomPassword(8);
-
-    document.getElementById('generatedStdID').innerText = studentId;
-    document.getElementById('generatedPwd').innerText = password;
-
-    var blob = new Blob(["Student ID: " + studentId + "\nPassword: " + password], { type: "text/plain" });
-
-    var a = document.createElement('a');
-        a.href = URL.createObjectURL(blob);
-        a.download = studentId + ".txt";
-        a.click();
-
-        document.getElementById('popup').style.display = 'block';
-}
-
-function btnPopup() {
-    window.location.href = '../student/login.html';
-}
-
-function getAlphabetPosition(letter) {
-    return letter.toUpperCase().charCodeAt(0) - 64;
-}
-
-function generateRandomPassword(length) {
-    var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var password = "";
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset.charAt(randomIndex);
-    }
-    return password;
-}
 function validateEmail() {
-    var emailInput = document.getElementById("email");
-    var emailError = document.getElementById("emailError");
+    const emailInput = document.getElementById("email");
+    const emailError = document.getElementById("emailError");
 
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var isValidEmail = emailPattern.test(emailInput.value);
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailPattern.test(emailInput.value);
 
     if (!isValidEmail) {
         emailError.classList.remove("hidden");
